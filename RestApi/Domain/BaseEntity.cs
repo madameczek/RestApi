8237e4e60f856d8a28a6bd13f1ece7ecb69d0d13
M-A <marek@adameczek.pl>
2022-09-29 16:11:09 +0200

    Add entity abstraction.

---
+++ RestApi/Domain/BaseEntity.cs
@@ -0,0 +1,10 @@
+﻿using System.Text.Json.Serialization;
+
+namespace RestApi.Domain;
+
+public abstract class BaseEntity<T>
+{
+    [JsonIgnore]
+    public T Id { get; set; } = default!;
+    public DateTime CreatedOn { get; set; }
+}
