8b85684f78e2647a1763392dc99fe02c272f368c
M-A <marek@adameczek.pl>
2022-09-29 16:12:52 +0200

    Add concrete entity and resource presented at system edge.

---
+++ RestApi/Domain/Customer.cs
@@ -0,0 +1,8 @@
+﻿namespace RestApi.Domain;
+
+public class Customer : BaseEntity<int>
+{
+    public string? LastName { get; set; }
+    public string? FirstName { get; set; }
+    public bool IsDeleted { get; set; }
+}
---
+++ RestApi/Resources/CustomerResource.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RestApi.Resources;
+
+public record CustomerResource(
+    [MinLength(2)]
+    [StringLength(200)]
+    string FirstName,
+    
+    [MinLength(2)]
+    [StringLength(200)]
+    string LastName,
+    
+    bool IsDeleted
+);
