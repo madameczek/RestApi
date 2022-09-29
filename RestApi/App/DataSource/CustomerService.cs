abc971a5c8df387642ed953d00658278ebde95ee
M-A <marek@adameczek.pl>
2022-09-29 17:21:58 +0200

    Implement customer service.

---
+++ RestApi/App/DataSource/CustomerService.cs
@@ -0,0 +1,26 @@
+﻿using RestApi.Resources;
+
+namespace RestApi.App.DataSource;
+
+public class CustomerService : ICustomerService
+{
+    public Task<int> Create(CustomerResource resource, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<CustomerResource> Get(int id, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task Update(string id, CustomerResource resource, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task Remove(string id, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
