f6e0fd96f4460f0ae5cbbd98f396d09121f06d40
M-A <marek@adameczek.pl>
2022-09-29 17:41:15 +0200

    Register customer service to DI container.

--- RestApi/Program.cs
+++ RestApi/Program.cs
@@ -1,3 +1,5 @@
+using RestApi.App.DataSource;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddScoped(typeof(ICustomerService), typeof(CustomerService));
 
 var app = builder.Build();
 
