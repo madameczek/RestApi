8118ed5dad40b51eb3a7089884e24965bdbd9d2b
M-A <marek@adameczek.pl>
2022-09-29 16:14:46 +0200

    Add crud service abstraction.

---
+++ RestApi/App/DataSource/ICustomerService.cs
@@ -0,0 +1,11 @@
+﻿using RestApi.Resources;
+
+namespace RestApi.App.DataSource;
+
+public interface ICustomerService
+{
+    Task<int> Create(CustomerResource customer, CancellationToken cancellationToken);
+    Task<CustomerResource> Get(int id, CancellationToken cancellationToken);
+    Task Update(string id, CustomerResource customer, CancellationToken cancellationToken);
+    Task Remove(string id, CancellationToken cancellationToken);
+}
