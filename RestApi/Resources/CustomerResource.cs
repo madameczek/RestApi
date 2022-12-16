3f1691267368908c160886310b4d705ff4df326c
M-A <marek@adameczek.pl>
2022-12-16 19:55:46 +0100

    Props are validated, so they are not null.

--- RestApi/Resources/CustomerResource.cs
+++ RestApi/Resources/CustomerResource.cs
@@ -5,12 +5,12 @@ namespace RestApi.Resources;
 public class CustomerResource
 {
     [MinLength(2)]
-    [StringLength(200)]
-    public string FirstName { get; set; }
+    [StringLength(200)] 
+    public string FirstName { get; set; } = null!;
     
     [MinLength(2)]
     [StringLength(200)]
-    public string LastName { get; set; }
+    public string LastName { get; set; } = null!;
     
     public bool IsDeleted { get; set; }
 }
